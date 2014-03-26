1f4b36b03f3597c542d2d07c88d7b3272a4064c8
Clemens Helm <clemens.helm@gmail.com>
2014-03-26 11:18:29 +0100

    Fixed specs.

--- spec/frontpage_spec.js
+++ spec/frontpage_spec.js
@@ -1,18 +1,30 @@
 var Browser = require("zombie");
 var app = require("../app");
 var browser = new Browser();;
-app.listen(3000, '127.0.0.1');
 
 describe("The frontpage", function () {
-  it("should declare that I've entered the Codeship", function () {
-    browser.visit("http://localhost:3000/", function () {
-      expect(browser.text("h1")).toEqual("I've entered the Codeship");
+  var server;
+
+  beforeEach(function () {
+    var listening = false;
+    var loaded = false;
+    runs(function() {
+      server = app.listen(3000, '127.0.0.1', function () { listening = true });
     });
+    waitsFor(function() { return listening }, "Couldn't start server.", 500);
+    runs(function() {
+      browser.visit("http://localhost:3000/").then(function () { loaded = true });
+    });
+    waitsFor(function() { return loaded }, "Couldn't load page.", 500);
+  });
+
+  afterEach(function () { server.close() });
+
+  it("should declare that I've entered the Codeship", function () {
+    expect(browser.text("h1")).toEqual("I've entered the Codeship!");
   });
 
   it("should lead to the Codeship", function () {
-    browser.visit("http://localhost:3000/", function () {
-      expect(browser.query("a[href='https://www.codeship.io']").text()).toEqual("codeship.io");
-    });
+    expect(browser.text("a[href='https://www.codeship.io']")).toEqual("codeship.io");
   });
 });
